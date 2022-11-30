860b83f9c13739bd09e983d1c4ff0c41511345db
Alex Levine <jewishalex1220@gmail.com>
2022-11-29 16:24:23 -0800

    Update exampleTest.js

--- exampleTest.js
+++ exampleTest.js
@@ -1,1 +1,8 @@
+import test from 'test'
 
+const test = require('test')
+
+test('synchronous passing test', t => {
+  // This test passes because it does not throw an exception.
+  assert.strictEqual(1, 1)
+})
