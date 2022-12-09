75e5060b7a3b801cea3fd561968d610d32f5252c
ukadir <usman1kadir@gmail.com>
2022-12-08 17:01:21 -0800

    unit tests

--- calorieTracker/unitTests/FoodCRUDUnitTest.test.js
+++ calorieTracker/unitTests/FoodCRUDUnitTest.test.js
@@ -14,3 +14,24 @@ test('input validation does not allow empty string', () => {
 test('input validation does not allow undefined', () => {
     expect(functions.inputValidation(undefined)).toBe(false);
 });
+
+test('save food to storage', () => {
+    var breakfastDiary = [{"foodName":"orange","mealType":"breakfast","calories":"50"}];
+    var lunchDiary = [{"foodName":"steak","mealType":"lunch","calories":"150"}];
+    var dinnerDiary = [{"foodName":"casserole","mealType":"dinner","calories":"100"}];
+
+    functions.saveFoodToDiary(breakfastDiary, lunchDiary, dinnerDiary);
+    expect(localStorage.getItem("breakfastDiary")).toBe(JSON.stringify([{"foodName":"orange","mealType":"breakfast","calories":"50"}]));
+    expect(localStorage.getItem("lunchDiary")).toBe(JSON.stringify([{"foodName":"steak","mealType":"lunch","calories":"150"}]));
+    expect(localStorage.getItem("dinnerDiary")).toBe(JSON.stringify([{"foodName":"casserole","mealType":"dinner","calories":"100"}]));
+});
+
+test('display food from storage', () => {
+    var breakfastDiary = [{"foodName":"orange","mealType":"breakfast","calories":"50"}];
+    var lunchDiary = [{"foodName":"steak","mealType":"lunch","calories":"150"}];
+    var dinnerDiary = [{"foodName":"casserole","mealType":"dinner","calories":"100"}];
+
+    functions.saveFoodToDiary(breakfastDiary, lunchDiary, dinnerDiary);
+    
+    expect(functions.getFoodFromStorage()).toStrictEqual([breakfastDiary, lunchDiary, dinnerDiary]);
+});
