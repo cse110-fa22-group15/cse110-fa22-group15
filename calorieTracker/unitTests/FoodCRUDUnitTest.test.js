16c8404dbfc520f0a56f9c893b5f44746f6b187f
ukadir <usman1kadir@gmail.com>
2022-12-08 18:43:41 -0800

    changes

--- calorieTracker/unitTests/FoodCRUDUnitTest.test.js
+++ calorieTracker/unitTests/FoodCRUDUnitTest.test.js
@@ -6,44 +6,44 @@ const functions = require('../calcumoleCode/Homepage.js');
 
 // TODO - Part 2
 test('input validation does not allow null value', () => {
-    expect(functions.inputValidation(null)).toBe(false);
+  expect(functions.inputValidation(null)).toBe(false);
 });
 
 test('input validation does not allow empty string', () => {
-    expect(functions.inputValidation('')).toBe(false);
+  expect(functions.inputValidation('')).toBe(false);
 });
 
 test('input validation does not allow undefined', () => {
-    expect(functions.inputValidation(undefined)).toBe(false);
+  expect(functions.inputValidation(undefined)).toBe(false);
 });
 
 test('save food to storage', () => {
-    var breakfastDiary = [{"foodName":"orange","mealType":"breakfast","calories":"50"}];
-    var lunchDiary = [{"foodName":"steak","mealType":"lunch","calories":"150"}];
-    var dinnerDiary = [{"foodName":"casserole","mealType":"dinner","calories":"100"}];
+  const breakfastDiary = [{ foodName: 'orange', mealType: 'breakfast', calories: '50' }];
+  const lunchDiary = [{ foodName: 'steak', mealType: 'lunch', calories: '150' }];
+  const dinnerDiary = [{ foodName: 'casserole', mealType: 'dinner', calories: '100' }];
 
-    functions.saveFoodToDiary(breakfastDiary, lunchDiary, dinnerDiary);
-    expect(localStorage.getItem("breakfastDiary")).toBe(JSON.stringify([{"foodName":"orange","mealType":"breakfast","calories":"50"}]));
-    expect(localStorage.getItem("lunchDiary")).toBe(JSON.stringify([{"foodName":"steak","mealType":"lunch","calories":"150"}]));
-    expect(localStorage.getItem("dinnerDiary")).toBe(JSON.stringify([{"foodName":"casserole","mealType":"dinner","calories":"100"}]));
+  functions.saveFoodToDiary(breakfastDiary, lunchDiary, dinnerDiary);
+  expect(localStorage.getItem('breakfastDiary')).toBe(JSON.stringify([{ foodName: 'orange', mealType: 'breakfast', calories: '50' }]));
+  expect(localStorage.getItem('lunchDiary')).toBe(JSON.stringify([{ foodName: 'steak', mealType: 'lunch', calories: '150' }]));
+  expect(localStorage.getItem('dinnerDiary')).toBe(JSON.stringify([{ foodName: 'casserole', mealType: 'dinner', calories: '100' }]));
 });
 
 test('display food from storage', () => {
-    var breakfastDiary = [{"foodName":"orange","mealType":"breakfast","calories":"50"}];
-    var lunchDiary = [{"foodName":"steak","mealType":"lunch","calories":"150"}];
-    var dinnerDiary = [{"foodName":"casserole","mealType":"dinner","calories":"100"}];
+  const breakfastDiary = [{ foodName: 'orange', mealType: 'breakfast', calories: '50' }];
+  const lunchDiary = [{ foodName: 'steak', mealType: 'lunch', calories: '150' }];
+  const dinnerDiary = [{ foodName: 'casserole', mealType: 'dinner', calories: '100' }];
 
-    functions.saveFoodToDiary(breakfastDiary, lunchDiary, dinnerDiary);
-    
-    expect(functions.getFoodFromStorage()).toStrictEqual([breakfastDiary, lunchDiary, dinnerDiary]);
+  functions.saveFoodToDiary(breakfastDiary, lunchDiary, dinnerDiary);
+
+  expect(functions.getFoodFromStorage()).toStrictEqual([breakfastDiary, lunchDiary, dinnerDiary]);
 });
 
 test('display food from storage', () => {
-    var breakfastDiary = [{"foodName":"orange","mealType":"breakfast","calories":"50"}];
-    var lunchDiary = [{"foodName":"steak","mealType":"lunch","calories":"150"}];
-    var dinnerDiary = [{"foodName":"casserole","mealType":"dinner","calories":"100"}];
+  const breakfastDiary = [{ foodName: 'orange', mealType: 'breakfast', calories: '50' }];
+  const lunchDiary = [{ foodName: 'steak', mealType: 'lunch', calories: '150' }];
+  const dinnerDiary = [{ foodName: 'casserole', mealType: 'dinner', calories: '100' }];
 
-    functions.addFoodsToDocument();
-    functions.saveFoodToDiary(breakfastDiary, lunchDiary, dinnerDiary);
-    expect(functions.getFoodFromStorage()).toStrictEqual([breakfastDiary, lunchDiary, dinnerDiary]);
+  functions.addFoodsToDocument();
+  functions.saveFoodToDiary(breakfastDiary, lunchDiary, dinnerDiary);
+  expect(functions.getFoodFromStorage()).toStrictEqual([breakfastDiary, lunchDiary, dinnerDiary]);
 });
